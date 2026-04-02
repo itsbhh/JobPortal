127fc3a29a8bb4a467ab5545575c27592e8e9b0a
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2026-04-02 08:37:23 +0530

    Update Applicants.jsx

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,37 +1,52 @@
-import React, { useEffect } from 'react'
-import Navbar from '../shared/Navbar'
-import ApplicantsTable from './ApplicantsTable'
-import axios from 'axios';
-import { APPLICATION_API_END_POINT } from '@/utils/constant';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setAllApplicants } from '@/redux/applicationSlice';
+import React, { useEffect } from "react";
+import Navbar from "../shared/Navbar";
+import ApplicantsTable from "./ApplicantsTable";
+import api from "@/api";
+import { APPLICATION_API_END_POINT } from "@/utils/constant";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAllApplicants } from "@/redux/applicationSlice";
 
 const Applicants = () => {
-    const params = useParams();
-    const dispatch = useDispatch();
-    const {applicants} = useSelector(store=>store.application);
+  const params = useParams();
+  const dispatch = useDispatch();
+  const { applicants } = useSelector((store) => store.application);
 
-    useEffect(() => {
-        const fetchAllApplicants = async () => {
-            try {
-                const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
-                dispatch(setAllApplicants(res.data.job));
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchAllApplicants();
-    }, []);
-    return (
-        <div>
-            <Navbar />
-            <div className='max-w-7xl mx-auto'>
-                <h1 className='font-bold text-xl my-5'>Applicants {applicants?.applications?.length}</h1>
-                <ApplicantsTable />
-            </div>
+  useEffect(() => {
+    const fetchAllApplicants = async () => {
+      try {
+        const res = await api.get(
+          `${APPLICATION_API_END_POINT}/${params.id}/applicants`,
+          { withCredentials: true }
+        );
+        dispatch(setAllApplicants(res.data.job));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllApplicants();
+  }, [params.id, dispatch]);
+
+  return (
+    <div>
+      <Navbar />
+
+      <div className="px-4 py-6">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="font-bold text-lg sm:text-xl mb-4">
+            Applicants{" "}
+            <span className="text-gray-500">
+              ({applicants?.applications?.length || 0})
+            </span>
+          </h1>
+
+          <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden">
+            <ApplicantsTable />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Applicants
+export default Applicants;
