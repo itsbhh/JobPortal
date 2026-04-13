4d765494efe4421a643978c3a28d6405511cf2ef
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2026-04-13 08:37:51 +0530

    Update Browse.jsx

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,39 +1,44 @@
-import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
-import Job from './Job';
-import { useDispatch, useSelector } from 'react-redux';
-import { setSearchedQuery } from '@/redux/jobSlice';
-import useGetAllJobs from '@/hooks/useGetAllJobs';
-
-// const randomJobs = [1, 2,45];
+import React, { useEffect } from "react";
+import Navbar from "./shared/Navbar";
+import Job from "./Job";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchedQuery } from "@/redux/jobSlice";
+import useGetAllJobs from "@/hooks/useGetAllJobs";
 
 const Browse = () => {
-    useGetAllJobs();
-    const {allJobs} = useSelector(store=>store.job);
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        return ()=>{
-            dispatch(setSearchedQuery(""));
-        }
-    },[])
-    return (
-        <div>
-            <Navbar />
-            <div className='max-w-7xl mx-auto my-10'>
-                <h1 className='font-bold text-xl my-10'>Search Results ({allJobs.length})</h1>
-                <div className='grid grid-cols-3 gap-4'>
-                    {
-                        allJobs.map((job) => {
-                            return (
-                                <Job key={job._id} job={job}/>
-                            )
-                        })
-                    }
-                </div>
+  useGetAllJobs();
+  const { allJobs } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
 
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    return () => {
+      dispatch(setSearchedQuery(""));
+    };
+  }, []);
 
-export default Browse
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+        <h1 className="font-bold text-lg sm:text-xl md:text-2xl mb-6">
+          Search Results ({allJobs.length})
+        </h1>
+
+        {allJobs.length === 0 ? (
+          <p className="text-center text-gray-500 mt-10">
+            No jobs found
+          </p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+            {allJobs.map((job) => (
+              <Job key={job._id} job={job} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Browse;
