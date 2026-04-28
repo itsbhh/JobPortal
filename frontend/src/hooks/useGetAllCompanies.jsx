b06e41c7235357d454bcc6d6c4d9a142eaeb9a35
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2026-04-28 14:17:13 +0530

    Update useGetAllCompanies.jsx

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -1,6 +1,7 @@
 import { setCompanies} from '@/redux/companySlice'
 import { COMPANY_API_END_POINT} from '@/utils/constant'
 import axios from 'axios'
+import api from '@/api';
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
@@ -9,7 +10,7 @@ const useGetAllCompanies = () => {
     useEffect(()=>{
         const fetchCompanies = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get`,{});
+                const res = await api.get(`${COMPANY_API_END_POINT}/get`,{});
                 console.log('called');
                 if(res.data.success){
                     dispatch(setCompanies(res.data.companies));
