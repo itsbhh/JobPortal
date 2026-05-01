1a8fe9142884770e0d4b84302e8ef3045154833c
Bhawna Anand <142158409+itsbhh@users.noreply.github.com>
2026-05-01 12:40:08 +0530

    Update useGetCompanyById.jsx

--- frontend/src/hooks/useGetCompanyById.jsx
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -2,6 +2,7 @@ import { setSingleCompany } from '@/redux/companySlice'
 import { setAllJobs } from '@/redux/jobSlice'
 import { COMPANY_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant'
 import axios from 'axios'
+import api from '@/api';
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
@@ -10,7 +11,7 @@ const useGetCompanyById = (companyId) => {
     useEffect(()=>{
         const fetchSingleCompany = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
+                const res = await api.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
                 console.log(res.data.company);
                 if(res.data.success){
                     dispatch(setSingleCompany(res.data.company));
